dfdd3447889d6b7aedaabb85cf86e83d6c151c11
Vitaly Alekseev <valekseev@yandex.ru>
2013-02-22 16:55:53 +0400

    saver232 init

---
+++ savers232/savers232/Form1.Designer.cs
@@ -0,0 +1,214 @@
+﻿namespace savers232
+{
+    partial class Form1
+    {
+        /// <summary>
+        /// Required designer variable.
+        /// </summary>
+        private System.ComponentModel.IContainer components = null;
+
+        /// <summary>
+        /// Clean up any resources being used.
+        /// </summary>
+        /// <param name="disposing">true if managed resources should be disposed; otherwise, false.</param>
+        protected override void Dispose(bool disposing)
+        {
+            if (disposing && (components != null))
+            {
+                components.Dispose();
+            }
+            base.Dispose(disposing);
+        }
+
+        #region Windows Form Designer generated code
+
+        /// <summary>
+        /// Required method for Designer support - do not modify
+        /// the contents of this method with the code editor.
+        /// </summary>
+        private void InitializeComponent()
+        {
+            this.button1 = new System.Windows.Forms.Button();
+            this.button2 = new System.Windows.Forms.Button();
+            this.richTextBox1 = new System.Windows.Forms.RichTextBox();
+            this.label1 = new System.Windows.Forms.Label();
+            this.label2 = new System.Windows.Forms.Label();
+            this.comboBox1 = new System.Windows.Forms.ComboBox();
+            this.button3 = new System.Windows.Forms.Button();
+            this.label3 = new System.Windows.Forms.Label();
+            this.textBox2 = new System.Windows.Forms.TextBox();
+            this.button4 = new System.Windows.Forms.Button();
+            this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
+            this.comboBox2 = new System.Windows.Forms.ComboBox();
+            this.SuspendLayout();
+            // 
+            // button1
+            // 
+            this.button1.Location = new System.Drawing.Point(528, 354);
+            this.button1.Name = "button1";
+            this.button1.Size = new System.Drawing.Size(180, 23);
+            this.button1.TabIndex = 0;
+            this.button1.Text = "open byte";
+            this.button1.UseVisualStyleBackColor = true;
+            this.button1.Click += new System.EventHandler(this.button1_Click);
+            // 
+            // button2
+            // 
+            this.button2.Location = new System.Drawing.Point(531, 413);
+            this.button2.Name = "button2";
+            this.button2.Size = new System.Drawing.Size(177, 23);
+            this.button2.TabIndex = 1;
+            this.button2.Text = "close";
+            this.button2.UseVisualStyleBackColor = true;
+            this.button2.Click += new System.EventHandler(this.button2_Click);
+            // 
+            // richTextBox1
+            // 
+            this.richTextBox1.Location = new System.Drawing.Point(27, 32);
+            this.richTextBox1.Name = "richTextBox1";
+            this.richTextBox1.Size = new System.Drawing.Size(492, 449);
+            this.richTextBox1.TabIndex = 3;
+            this.richTextBox1.Text = "";
+            // 
+            // label1
+            // 
+            this.label1.AutoSize = true;
+            this.label1.Location = new System.Drawing.Point(525, 61);
+            this.label1.Name = "label1";
+            this.label1.Size = new System.Drawing.Size(26, 13);
+            this.label1.TabIndex = 4;
+            this.label1.Text = "Port";
+            // 
+            // label2
+            // 
+            this.label2.AutoSize = true;
+            this.label2.Location = new System.Drawing.Point(525, 96);
+            this.label2.Name = "label2";
+            this.label2.Size = new System.Drawing.Size(33, 13);
+            this.label2.TabIndex = 5;
+            this.label2.Text = "Parity";
+            // 
+            // comboBox1
+            // 
+            this.comboBox1.FormattingEnabled = true;
+            this.comboBox1.Items.AddRange(new object[] {
+            "none",
+            "odd",
+            "even",
+            "space",
+            "mark"});
+            this.comboBox1.Location = new System.Drawing.Point(579, 96);
+            this.comboBox1.Name = "comboBox1";
+            this.comboBox1.Size = new System.Drawing.Size(121, 21);
+            this.comboBox1.TabIndex = 6;
+            // 
+            // button3
+            // 
+            this.button3.Location = new System.Drawing.Point(531, 258);
+            this.button3.Name = "button3";
+            this.button3.Size = new System.Drawing.Size(75, 23);
+            this.button3.TabIndex = 7;
+            this.button3.Text = "button3";
+            this.button3.UseVisualStyleBackColor = true;
+            this.button3.Click += new System.EventHandler(this.button3_Click_1);
+            // 
+            // label3
+            // 
+            this.label3.AutoSize = true;
+            this.label3.Location = new System.Drawing.Point(528, 140);
+            this.label3.Name = "label3";
+            this.label3.Size = new System.Drawing.Size(110, 13);
+            this.label3.TabIndex = 8;
+            this.label3.Text = "Read timeout (milisec)";
+            this.label3.Click += new System.EventHandler(this.label3_Click);
+            // 
+            // textBox2
+            // 
+            this.textBox2.Location = new System.Drawing.Point(579, 156);
+            this.textBox2.Name = "textBox2";
+            this.textBox2.Size = new System.Drawing.Size(95, 20);
+            this.textBox2.TabIndex = 9;
+            // 
+            // button4
+            // 
+            this.button4.Location = new System.Drawing.Point(531, 449);
+            this.button4.Name = "button4";
+            this.button4.Size = new System.Drawing.Size(75, 23);
+            this.button4.TabIndex = 10;
+            this.button4.Text = "Clear";
+            this.button4.UseVisualStyleBackColor = true;
+            this.button4.Click += new System.EventHandler(this.button4_Click);
+            // 
+            // button5
+            // 
+            this.button5.Location = new System.Drawing.Point(528, 384);
+            this.button5.Name = "button5";
+            this.button5.Size = new System.Drawing.Size(180, 23);
+            this.button5.TabIndex = 11;
+            this.button5.Text = "open command";
+            this.button5.UseVisualStyleBackColor = true;
+            this.button5.Click += new System.EventHandler(this.button5_Click);
+            // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(625, 449);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(75, 23);
+            this.button6.TabIndex = 12;
+            this.button6.Text = "Exit";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
+            // comboBox2
+            // 
+            this.comboBox2.FormattingEnabled = true;
+            this.comboBox2.Location = new System.Drawing.Point(579, 61);
+            this.comboBox2.Name = "comboBox2";
+            this.comboBox2.Size = new System.Drawing.Size(121, 21);
+            this.comboBox2.TabIndex = 13;
+            // 
+            // Form1
+            // 
+            this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
+            this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
+            this.ClientSize = new System.Drawing.Size(720, 493);
+            this.Controls.Add(this.comboBox2);
+            this.Controls.Add(this.button6);
+            this.Controls.Add(this.button5);
+            this.Controls.Add(this.button4);
+            this.Controls.Add(this.textBox2);
+            this.Controls.Add(this.label3);
+            this.Controls.Add(this.button3);
+            this.Controls.Add(this.comboBox1);
+            this.Controls.Add(this.label2);
+            this.Controls.Add(this.label1);
+            this.Controls.Add(this.richTextBox1);
+            this.Controls.Add(this.button2);
+            this.Controls.Add(this.button1);
+            this.Name = "Form1";
+            this.Text = "Form1";
+            this.Load += new System.EventHandler(this.Form1_Load);
+            this.ResumeLayout(false);
+            this.PerformLayout();
+
+        }
+
+        #endregion
+
+        private System.Windows.Forms.Button button1;
+        private System.Windows.Forms.Button button2;
+        private System.Windows.Forms.RichTextBox richTextBox1;
+        private System.Windows.Forms.Label label1;
+        private System.Windows.Forms.Label label2;
+        private System.Windows.Forms.ComboBox comboBox1;
+        private System.Windows.Forms.Button button3;
+        private System.Windows.Forms.Label label3;
+        private System.Windows.Forms.TextBox textBox2;
+        private System.Windows.Forms.Button button4;
+        private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
+        private System.Windows.Forms.ComboBox comboBox2;
+    }
+}
+
---
+++ savers232/savers232/Form1.cs
@@ -0,0 +1,296 @@
+﻿using System;
+using System.Collections.Generic;
+using System.ComponentModel;
+using System.Data;
+using System.Drawing;
+using System.Linq;
+using System.Text;
+using System.Windows.Forms;
+using System.IO;
+using System.IO.Ports;
+using System.Threading;
+using System.ComponentModel;
+
+namespace savers232
+{
+
+
+    public partial class Form1 : Form
+    {
+        public SerialPort serialPort;
+        public FileStream fs;
+        public StreamWriter sr;
+        private Thread th;
+
+        public Form1()
+        {
+            InitializeComponent();
+
+            comboBox1.SelectedIndex = 0;
+            //textBox1.Text = "18";
+            textBox2.Text = "25";
+
+            string[] ports = SerialPort.GetPortNames();
+           // Array.Sort(ports);
+            comboBox2.Items.AddRange(ports);
+            if(ports.Length > 0)
+                comboBox2.SelectedIndex = 0;
+        }
+
+
+        public void AddText(int _str)
+        {
+            this.richTextBox1.Text += Convert.ToChar(_str);
+        }
+
+        public void AddText(string _str)
+        {
+            this.richTextBox1.Text += _str;
+        }
+
+        private int GetReadTimeout()
+        {
+            int timeout = 25;
+            try
+            {
+                timeout = Convert.ToInt32(textBox2.Text);
+            }
+            catch(Exception ee)
+            {
+            }
+
+            return timeout;
+        }
+ 
+        private Parity GetParity()
+        {
+            Parity ret = Parity.None;
+
+            switch(comboBox1.SelectedIndex)
+            {
+                case 0:
+                    ret = Parity.None;
+                    break;
+                case 1:
+                    ret = Parity.Odd;
+                    break;
+                case 2:
+                    ret = Parity.Even;
+                    break;
+                case 3:
+                    ret = Parity.Space;
+                    break;
+                case 4:
+                    ret = Parity.Mark;
+                    break;
+                default:
+                    ret = Parity.None;
+                    break;
+                 
+            }
+
+            return ret;
+
+        }
+
+        private void button1_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                //открытие COM порта
+                serialPort = new SerialPort();
+                serialPort.PortName = comboBox2.Text.Trim();
+                serialPort.BaudRate = 9600;
+                serialPort.Parity = GetParity();
+
+                serialPort.Open();
+                richTextBox1.Text += "Port " + comboBox2.Text.Trim() + " is open\n";
+
+                fs = new FileStream(@"c:\tmp\mdb.log", FileMode.Append,FileAccess.Write);
+
+                richTextBox1.Text += "File " + @"c:\tmp\mdb.log" + " is open\n";
+
+                richTextBox1.Text += "Creating thread\n";
+
+                th = new Thread(Work.DoWork);
+                th.Start(this);
+
+                richTextBox1.Text += "Thread was created\n";
+
+
+            }
+            catch(Exception ex)
+            {
+                richTextBox1.Text += "\n Exception: " + ex.Message;
+            }
+
+        }
+
+        private void button2_Click(object sender, EventArgs e)
+        {
+
+            Work.cont = false;
+            this.Cursor = Cursors.WaitCursor;
+            Thread.Sleep(500);
+            this.Cursor = Cursors.Arrow;
+
+            try { th.Abort(); }
+            catch (Exception tt) { }
+            
+            richTextBox1.Text += "\nThread closed\n";
+
+            try { fs.Close(); }catch (Exception tt){}
+            richTextBox1.Text += "File closed\n";
+
+            try
+            {
+                if (serialPort != null && serialPort.IsOpen)
+                    serialPort.Close();
+            }
+            catch (Exception tt) { }
+
+            richTextBox1.Text += "Port closed\n";
+
+        }
+
+        private void button3_Click(object sender, EventArgs e)
+        {
+ 
+        }
+
+        private void button3_Click_1(object sender, EventArgs e)
+        {
+
+            ASCIIEncoding ascii = new ASCIIEncoding();
+            //Byte[] encodedBytes = ascii.GetBytes(unicodeString);
+            //String decodedString = ascii.GetString(encodedBytes);
+
+            byte[] in_buff = new byte[512];
+            in_buff[0] = (byte)'1';
+            in_buff[1] = (byte)'2';
+            in_buff[2] = (byte)'3';
+            in_buff[3] = (byte)'6';
+            in_buff[4] = (byte)'t';
+
+                
+
+            String str = ascii.GetString(in_buff, 0, 5);
+            richTextBox1.Text += "string" + str;
+
+        }
+        
+        private void label3_Click(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button4_Click(object sender, EventArgs e)
+        {
+            richTextBox1.Clear();
+        }
+
+        private void button5_Click(object sender, EventArgs e)
+        {
+            try
+            {
+
+                //открытие COM порта
+                serialPort = new SerialPort();
+                serialPort.PortName = comboBox2.Text.Trim();
+                serialPort.BaudRate = 9600;
+                serialPort.Parity = GetParity();
+
+                serialPort.Open();
+                richTextBox1.Text += "Port " + comboBox2.Text.Trim() + " is open\n";
+
+                fs = new FileStream(@"c:\tmp\mdb.log", FileMode.Append, FileAccess.Write);
+
+                richTextBox1.Text += "File " + @"c:\tmp\mdb.log" + " is open\n";
+
+                richTextBox1.Text += "Creating thread\n";
+
+                th = new Thread(Work.DoWorkCommand);
+                th.Start(this);
+
+                richTextBox1.Text += "Thread was created\n";
+
+
+            }
+            catch (Exception ex)
+            {
+                richTextBox1.Text += "\n Exception: " + ex.Message;
+            }
+
+        }
+
+        private void Form1_Load(object sender, EventArgs e)
+        {
+
+        }
+
+        private void button6_Click(object sender, EventArgs e)
+        {
+            Application.Exit();
+        }
+    }
+
+    public class Work
+    {
+        public static Boolean cont = true;
+
+        static byte[] GetBytes(string str)
+        {
+            byte[] bytes = new byte[str.Length * sizeof(char)];
+            System.Buffer.BlockCopy(str.ToCharArray(), 0, bytes, 0, bytes.Length);
+            return bytes;
+        }
+
+        // public static int ThreadProc(Form1 _form)
+        public static void DoWork(object data)
+        {
+
+            cont = true;
+            do
+            {
+                if (((Form1)data).serialPort.BytesToRead > 0)
+                {
+                    int rbyte = ((Form1)data).serialPort.ReadByte();
+                    ((Form1)data).AddText(rbyte);
+                    ((Form1)data).fs.WriteByte((byte)rbyte);
+                }
+                Thread.Sleep(10);
+            } while (cont);
+
+        }
+
+        // public static int ThreadProc(Form1 _form)
+        public static void DoWorkCommand(object data)
+        {
+            byte[] in_buff = new byte[512];
+            int read_size=0;
+
+            cont = true;
+            do
+            {
+                read_size = 0;
+                if (((Form1)data).serialPort.BytesToRead > 0)
+                {
+                    read_size = ((Form1)data).serialPort.Read(in_buff, 0, 512);
+                    if (read_size > 0)
+                    {
+                        String str;
+                        ASCIIEncoding ascii = new ASCIIEncoding();
+                        str = ascii.GetString(in_buff, 0, read_size);
+
+                        ((Form1)data).AddText("Command - " + str + "\n");
+                        ((Form1)data).fs.Write(in_buff,0,read_size);
+                    }
+               }
+               Thread.Sleep(70);
+            } while (cont);
+
+        }
+    }
+
+}
